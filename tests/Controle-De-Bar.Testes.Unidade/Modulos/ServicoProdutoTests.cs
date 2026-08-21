054f4b1d493ef470c41e9fdd33e9fb6f5e1ed7c3
Kauan Silva <kauanmacedo338@gmail.com>
2026-08-21 15:03:59 -0300

    feat: enhance ServicoProdutoTests with additional validation and editing scenarios

--- tests/Controle-De-Bar.Testes.Unidade/Modulos/ServicoProdutoTests.cs
+++ tests/Controle-De-Bar.Testes.Unidade/Modulos/ServicoProdutoTests.cs
@@ -5,7 +5,6 @@ using Moq;
 
 namespace Controle_De_Bar.Testes.Unidade.Modulos;
 
-
 [TestClass]
 public sealed class ServicoProdutoTests
 {
@@ -21,7 +20,7 @@ public sealed class ServicoProdutoTests
 
         Produto? produtoCadastrado = null;
 
-        repositorioProduto.Setup(r => r.Cadastrar(It.IsAny<Produto>())).Callback<Produto>(materia => produtoCadastrado = materia);
+        repositorioProduto.Setup(r => r.Cadastrar(It.IsAny<Produto>())).Callback<Produto>(produto => produtoCadastrado = produto);
 
         ServicoProduto servicoProduto = new ServicoProduto(
             repositorioProduto.Object
@@ -38,4 +37,75 @@ public sealed class ServicoProdutoTests
 
         repositorioProduto.Verify(r => r.Cadastrar(It.IsAny<Produto>()), Times.Once);
     }
+
+    [TestMethod]
+    public void Cadastrar_DadosInvalidos_RetornaErro()
+    {
+        // Arrange
+        Mock<IRepositorioProduto> repositorioProduto = new();
+
+        repositorioProduto
+            .Setup(r => r.SelecionarTodos())
+            .Returns([]);
+
+        ServicoProduto servicoProduto = new(
+            repositorioProduto.Object
+        );
+
+        // Act
+        Result resultado = servicoProduto
+            .Cadastrar(new CadastrarProdutoDto(string.Empty, 10));
+
+        // Assert
+        Assert.IsTrue(resultado.IsFailed);
+        Assert.AreEqual(
+            "O Campo Nome precisa ser preenchido;",
+            resultado.Errors.First().Message
+        );
+
+        repositorioProduto.Verify(
+            r => r.Cadastrar(It.IsAny<Produto>()),
+            Times.Never
+        );
+    }
+
+
+    [TestMethod]
+    public void Editar_PrecoProdutoCadastrado_NovoPrecoESalvoCorretamente()
+    {
+        // Arrange
+        Produto produto = new("Produto Teste", 10);
+
+        Mock<IRepositorioProduto> repositorioProduto = new();
+
+        repositorioProduto
+            .Setup(r => r.SelecionarPorId(produto.Id))
+            .Returns(produto);
+
+        repositorioProduto
+            .Setup(r => r.Editar(produto.Id, It.IsAny<Produto>()))
+            .Returns(true);
+
+        ServicoProduto servicoProduto = new(
+            repositorioProduto.Object
+        );
+
+        // Act
+        Result resultado = servicoProduto.Editar(
+            new EditarProdutoDto(produto.Id, "Produto Teste", 20)
+        );
+
+        // Assert
+        Assert.IsTrue(resultado.IsSuccess);
+
+        repositorioProduto.Verify(
+            r => r.Editar(
+                produto.Id,
+                It.Is<Produto>(p => p.Preco == 20)
+            ),
+            Times.Once
+        );
+    }
+
+
 }

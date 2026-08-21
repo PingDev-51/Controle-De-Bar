15788f190551eebb45b84eacd0c9fbdcb4610d51
Kauan Silva <kauanmacedo338@gmail.com>
2026-08-21 14:30:30 -0300

    feat: add unit tests for Produto class including validation and update scenarios

--- tests/Controle-De-Bar.Testes.E2E/Compartilhado/E2ETestsBase.cs
+++ tests/Controle-De-Bar.Testes.E2E/Compartilhado/E2ETestsBase.cs
@@ -1,36 +1,36 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Playwright;
-using Microsoft.Playwright.MSTest;
+// using Microsoft.AspNetCore.Identity;
+// using Microsoft.Extensions.DependencyInjection;
+// using Microsoft.Playwright;
+// using Microsoft.Playwright.MSTest;
 
-namespace eAgenda.Testes.E2E.Compartilhado;
+// namespace eAgenda.Testes.E2E.Compartilhado;
 
-public abstract class E2ETestsBase : PageTest
-{
-    private TestApplicationFactory aplicacao = null!;
+// public abstract class E2ETestsBase : PageTest
+// {
+//     private TestApplicationFactory aplicacao = null!;
 
-    protected string UrlBase { get; set; } = string.Empty;
+//     protected string UrlBase { get; set; } = string.Empty;
 
-    [TestInitialize]
-    public async Task InicializarAplicacao()
-    {
-        aplicacao = new TestApplicationFactory();
+//     [TestInitialize]
+//     public async Task InicializarAplicacao()
+//     {
+//         aplicacao = new TestApplicationFactory();
 
-        UrlBase = aplicacao.UrlBase;
-    }
+//         UrlBase = aplicacao.UrlBase;
+//     }
 
-    [TestCleanup]
-    public async Task EncerrarAplicacao()
-    {
-        try
-        {
-            if (aplicacao is not null)
-                await aplicacao.DisposeAsync();
-        }
-        finally
-        {
-            aplicacao = null!;
-        }
-    }
+//     [TestCleanup]
+//     public async Task EncerrarAplicacao()
+//     {
+//         try
+//         {
+//             if (aplicacao is not null)
+//                 await aplicacao.DisposeAsync();
+//         }
+//         finally
+//         {
+//             aplicacao = null!;
+//         }
+//     }
 
-}
+// }

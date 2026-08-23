b9dd5e6eeaf408c11252899bd52d8ac551ea3d04
Kauan Galvani <kauanrafaelgalvani@gmail.com>
2026-08-22 22:22:53 -0300

    Add: Testes de Mesa

---
+++ tests/Controle-De-Bar.Testes.Unidade/Modulos/ModuloMesa/MesaTests.cs
@@ -0,0 +1,85 @@
+using System;
+using ControleDeBar.Dominio.Modulos.ModuloMesa;
+
+namespace Controle_De_Bar.Testes.Unidade.Modulos.ModuloMesa;
+
+[TestClass]
+public sealed class MesaTests()
+{
+    [TestMethod]
+    public void CadastrarMesa_ComTodosOsCamposPreenchidos()
+    {
+        // Arrange
+        Mesa mesa = new Mesa(
+            "Testar",
+            "Testar",
+            StatusDaMesa.Indefiniodo
+        );
+
+        // Act
+        List<string> erros = mesa.Validar();
+
+        // Assert
+        Assert.HasCount(0, erros);
+    }
+
+    [TestMethod]
+    public void CadastrarProduto_ComONumeroDaMesaInvalido()
+    {
+        // Arrange
+        Mesa mesa = new Mesa()
+        {
+            QuantidadeDeLugares = "1"
+        };
+
+        // Act
+        List<string> erros = mesa.Validar();
+
+        // Assert
+        Assert.HasCount(1, erros);
+        Assert.AreEqual(
+            "O Campo Numero Da Mesa precisa ser preenchido;",
+            erros.First()
+        );
+    }
+
+    [TestMethod]
+    public void CadastrarProduto_ComOQuantidadeDeLugaresInvalido()
+    {
+        // Arrange
+        Mesa mesa = new Mesa()
+        {
+            NumeroDaMesa = "1"
+        };
+
+        // Act
+        List<string> erros = mesa.Validar();
+
+        // Assert
+        Assert.HasCount(1, erros);
+        Assert.AreEqual(
+            "O Campo Quantidade De Lugares precisa ser preenchido;",
+            erros.First()
+        );
+    }
+
+    [TestMethod]
+    public void AtualizarProduto()
+    {
+        // Arrange
+        Mesa mesa = new Mesa(
+            "Testar",
+            "Testar",
+            StatusDaMesa.Indefiniodo
+        );
+
+        Mesa mesaAtualizada = new Mesa("TestarAtualizado", "TestarAtualizado", StatusDaMesa.Indefiniodo);
+
+        // Act
+        mesa.Atualizar(mesaAtualizada);
+        List<string> erros = mesa.Validar();
+
+        // Assert
+        Assert.HasCount(0, erros);
+    }
+}
---
+++ tests/Controle-De-Bar.Testes.Unidade/Modulos/ModuloMesa/ServicoMesaTests.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Controle_De_Bar.Testes.Unidade.Modulos.ModuloMesa;
+
+public class ServicoMesaTests
+{
+
+}

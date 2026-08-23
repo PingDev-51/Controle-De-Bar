b4ed6e14737b146504e2c34c6ab7fee9b42583b8
Kauan Galvani <kauanrafaelgalvani@gmail.com>
2026-08-23 02:36:32 -0300

    Add: testes de integração do ModuloMesa

--- tests/Controle-De-Bar.Testes.Integracao/Compartilhado/Orm/RepositorioBaseOrmTests.cs
+++ tests/Controle-De-Bar.Testes.Integracao/Compartilhado/Orm/RepositorioBaseOrmTests.cs
@@ -6,6 +6,8 @@ using FizzWare.NBuilder;
 using Microsoft.EntityFrameworkCore;
 using GeradorDeProvas.Infra.Compartilhado.Orm;
 using Controle_De_Bar.Testes.Integracao.Compartilhado.Identity;
+using ControleDeBar.Infra.Modulos.ModuloMesa;
+using ControleDeBar.Dominio.Modulos.ModuloMesa;
 
 namespace ControleDeBar.Testes.Integracao.Compartilhado.Orm;
 
@@ -13,6 +15,7 @@ public abstract class RepositorioBaseEmOrmTests
 {
     protected ControleDeBarDbContext dbContext = null!;
     protected RepositorioProdutoEmOrm repositorioProduto = null!;
+    protected RepositorioMesaEmOrm repositorioMesa = null!;
 
     protected Guid userId;
 
@@ -25,15 +28,26 @@ public abstract class RepositorioBaseEmOrmTests
 
         // Produto
         repositorioProduto = new RepositorioProdutoEmOrm(dbContext);
+        repositorioMesa = new RepositorioMesaEmOrm(dbContext);
 
         BuilderSetup.SetCreatePersistenceMethod<Produto>(
             repositorioProduto.Cadastrar
+        );
+
+        BuilderSetup.SetCreatePersistenceMethod<Mesa>(
+            repositorioMesa.Cadastrar
         );
 
         BuilderSetup.SetCreatePersistenceMethod<IList<Produto>>((produtos) =>
         {
             foreach (Produto p in produtos)
                 repositorioProduto.Cadastrar(p);
+        });
+
+        BuilderSetup.SetCreatePersistenceMethod<IList<Mesa>>((mesa) =>
+        {
+            foreach (Mesa m in mesa)
+                repositorioMesa.Cadastrar(m);
         });
     }
 
